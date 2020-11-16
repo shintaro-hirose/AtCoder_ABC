42d625825e95215b06fec9b2eb692bb1c8864a7a
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-11-16 22:39:18 +0900

    Add ABC183 F

--- ABC169-183/ABC183/F.cpp
+++ ABC169-183/ABC183/F.cpp
@@ -18,71 +18,49 @@ typedef vector<vb> vvb;
 #define PI (acos(-1))
 const ll mod = 1000000007;
 
+
 struct UnionFind {
-    vector<int> par; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
-    
-    UnionFind(int N) : par(N) { //最初は全てが根であるとして初期化
-        for(int i = 0; i < N; i++) par[i] = i;
-    }
-    
-    int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
-        if (par[x] == x) return x;
+    vector<int> par;
+    vector<map<int, int>> mp;
+
+    UnionFind(int n): par(n,-1), mp(n) {}
+    int root(int x) {
+        if (par[x] < 0) return x;
         return par[x] = root(par[x]);
     }
-    
-    void unite(int x, int y) { // xとyの木を併合
-        int rx = root(x); //xの根をrx
-        int ry = root(y); //yの根をry
-        if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
-        par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+    bool unite(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x,y);
+        for(auto p: mp[y]){
+            mp[x][p.first] += p.second;
+        }
+        mp[y] = map<int, int>();
+        par[x] += par[y];
+        par[y] = x;
+        return true;
     }
-    
-    bool same(int x, int y) { // 2つのデータx, yが属する木が同じならtrueを返す
-        int rx = root(x);
-        int ry = root(y);
-        return rx == ry;
-    }
+    bool same(int x, int y) { return root(x) == root(y);}
+    int size(int x) { return -par[root(x)];}
 };
 
 int main() {
     int N, Q; cin >> N >> Q;
-    vi C(N);
-    rep(i, N) {
-        cin >> C[i];
-        C[i]--;
-    }
-
     UnionFind uf(N);
-
-    vector<unordered_map<int, int>> V(N, unordered_map<int, int>(0));
-
-    rep(i, N){
-        V[uf.root(i)][C[i]]++;
+    rep(i, N) {
+        int c; cin >> c;
+        uf.mp[i][c] = 1;
     }
     rep(i, Q){
-        int q, a, b; cin >> q >> a >> b;
-        if(q == 1){
+        int x, a, b; cin >> x >> a >> b;
+        if(x == 1){
             a--; b--;
-            int aroot = uf.root(a);
-            int broot = uf.root(b);
-            if(aroot != broot){
-                uf.unite(a, b);
-                int bothroot = uf.root(b);
-                for(auto itr = V[aroot].begin(); itr != V[aroot].end(); itr++){
-                    V[bothroot][(*itr).first] += (*itr).second;
-                    // cout << "********" << endl;
-                    // cout << (*itr).first << endl;
-                    // cout << (*itr).second << endl;
-                    // cout << "********" << endl;
-                    // cout << V[bothroot][(*itr).first] << endl;
-                    // cout << endl;
-                }
-            }
-            
+            uf.unite(a,b);
         } else {
-            a--;b--;
-            cout << V[uf.root(a)][b] << endl;
+            a--;
+            cout << uf.mp[uf.root(a)][b] << endl;
         }
     }
+
     return 0;
 }
